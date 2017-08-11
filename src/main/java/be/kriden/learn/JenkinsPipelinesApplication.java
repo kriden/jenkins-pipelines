58195ac8591b3e5744dc15bdb9612241ef9dfc8e
Kristof De Nolf <kristof.denolf@amplexor.com>
2017-08-11 23:18:18 +0200

    basic spring boot project

---
+++ src/main/java/be/kriden/learn/JenkinsPipelinesApplication.java
@@ -0,0 +1,12 @@
+package be.kriden.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelinesApplication.class, args);
+	}
+}
