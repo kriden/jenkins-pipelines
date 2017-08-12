30cee5d140b9b5a0a1bdd4918dc88783a2102cfb
Kristof De Nolf <kristof.denolf@amplexor.com>
2017-08-12 10:15:26 +0200

    Fail quality gate!

--- src/main/java/be/kriden/learn/JenkinsPipelinesApplication.java
+++ src/main/java/be/kriden/learn/JenkinsPipelinesApplication.java
@@ -9,5 +9,11 @@ public class JenkinsPipelinesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPipelinesApplication.class, args);
 		// TODO Implement application
+
+		int number = 3;
+		int target = 5;
+
+		target =- number;
+		target =+ number;
 	}
 }
